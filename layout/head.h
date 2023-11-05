ac6f8ac9c428ef76c49591ccd964f763d9bec2a5
LacLic <281885421@qq.com>
2023-11-05 12:07:01 +0800

    add wiring layout

---
+++ layout/head.h
@@ -0,0 +1,10 @@
+#ifndef _HEAD_H_
+#define _HEAD_H_
+#include <fstream>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <functional>
+#include <queue>
+#include <map>
+#endif
---
+++ layout/main.cpp
@@ -0,0 +1,51 @@
+#include "head.h"
+
+int sqrt(int x) {
+    int l = 0, r = x, res = 0;
+    while(l <= r) {
+        int mid = l + r >> 1;
+        if (mid * mid >= x) {
+            r = mid - 1;
+            res = mid;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return res;
+}
+
+std::vector<int> get_network(std::string filename) {
+    std::ifstream network_fs(filename);
+
+    std::vector<int> mat;
+    int x;
+    while (network_fs >> x) {
+        mat.emplace_back(x);
+    }
+    network_fs.close();
+
+    return mat;
+}
+
+std::string filename_prefix() {
+    std::string input_id = "1";
+    // std::cin >> input_id;
+    const std::string prefix = "./TestCase20230927/testcase";
+    const std::string suffix = "/design.";
+    return prefix + input_id + suffix;
+}
+
+int main() {
+    assert(sqrt(16384) == 128);
+
+    std::vector<int> adj;
+    get_network(filename_prefix() + "die.network").swap(adj);
+    int n = sqrt(adj.size());
+    auto weight = [&](int x, int y) {
+        return adj[x * n + y];
+    };
+
+    std::function<void(void)> search = [&]() {
+        
+    };
+}
