71e00c64751c43b6b49c6f29571f730508c040c0
LacLic <281885421@qq.com>
2023-11-09 18:40:06 +0800

    reconstruct global.h

--- src/global.h
+++ src/global.h
@@ -1,17 +1,5 @@
 #ifndef _GLOBAL_H_
 #define _GLOBAL_H_
-#include <string>
-#include <vector>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <iostream>
-#include <queue>
-#include <math.h>
-#include <algorithm>
-#include <unordered_map>
-#include <unordered_set>
-#include <iostream>
-#include <fstream>
+#include "global_head.h"
 using namespace std;
 #endif
---
+++ src/global_head.h
@@ -0,0 +1,16 @@
+#ifndef _GLOBAL_HEAD_H_
+#define _GLOBAL_HEAD_H_
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <iostream>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+#include <fstream>
+#endif
