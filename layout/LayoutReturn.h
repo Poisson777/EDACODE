71b27a03fd4b39010e14f43b1313c0a63872b6ad
LacLic <281885421@qq.com>
2023-11-12 11:12:44 +0800

    add annotation to LayoutReturn.h

--- layout/LayoutReturn.h
+++ layout/LayoutReturn.h
@@ -1,13 +1,25 @@
 #ifndef _LAYOUT_RETURN_H_
 #define _LAYOUT_RETURN_H_
+
 #include <vector>
 #include <string>
+
 struct LayoutReturn {
     int die_count;
     int FPGA_count;
-    std::vector<int> adjacent;
-    std::vector<int> die_to_FPGA;
+    std::vector<int> adjacent; // adjacent matrix
+    // usage: adjacent[i * die_count + j]
+    std::vector<int> die_to_FPGA; // die_index -> FPGA_index
+    // usage: die_to_FPGA[die_index]
     std::vector<std::pair<int, std::vector<std::pair<int,int>>>> networks;
+/*  vector<                              // networks
+        pair< network_id,                // network < net_id, edges >
+            vector<                      // edges
+                pair< from_die, to_die > // edge
+            >
+        >
+    > */
 };
 LayoutReturn layout(std::string testcase);
+
 #endif
